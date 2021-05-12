4c072e3f2468642c0d95ba3275266638320bebe8
Tonia <tonia.saba@gmail.com>
2021-05-12 17:32:48 -0400

    remove_dups method

--- coding_challenges/is_prime.rb
+++ coding_challenges/is_prime.rb
@@ -9,3 +9,19 @@ end
 p prime? 6
 
 p prime? 2
+# ************************************************
+array = [1,2,3,4,5,6,7,8,9,10]
+
+
+def count_prime(array)
+count = 0
+
+  array.each do |num|
+    next if num.prime? == false
+
+    if num.prime? == true
+      count += 1
+    end
+  end
+  p count
+end
